3e9329d22748c75ee5a64101b8b3d236a0b88fd4
Caterpillow <jola.mozyrska@gmail.com>
2020-04-30 22:26:02 +0200

    add tests

--- airline_page/main.js
+++ airline_page/main.js
@@ -1,62 +1,80 @@
-const listOfFlights = document.querySelectorAll('tr[data-id-lotu]');
-let mxNr = 0;
-for (const elem of listOfFlights) {
-    const str = elem.getAttribute("data-id-lotu");
-    const nr = +str;
-    if (mxNr < nr)
-        mxNr = nr;
+// function paint_elem(ev: MouseEvent) {
+//     let target = ev.target;
+//     let elem = this as HTMLElement;
+//     console.log(elem, target);
+//     let currentColor = window.getComputedStyle(elem).getPropertyValue('background-color');
+//     let [_,...colorsAsText] = /rgb\((\d+),[^0-9]*(\d+),[^0-9]*(\d+)\)/.exec(currentColor);
+//     let colors: number[] = [];
+//     for(let i = 0; i < 3; i++) colors[i] = (parseInt(colorsAsText[i]) + 0x20) % 256;
+//     elem.style.backgroundColor = `rgb(${colors[0]},${colors[1]},${colors[2]})`;
+// }
+// delayed_element.addEventListener('click', paint_elem);
+// reserved_element.addEventListener('click', paint_elem);
+//==============================================================================
+var delayed_element = document.getElementById('grid_right_column');
+var reserved_element = document.getElementById('grid_form');
+document.getElementsByTagName('div')[0].addEventListener('click', pokoloruj2);
+function pokoloruj2(ev) {
+    var target = ev.target;
+    if (delayed_element.contains(target) || reserved_element.contains(target))
+        console.log('trafilismy');
+    else
+        console.log('pudlo');
 }
-console.log(mxNr);
-function wait(ms) {
-    return new Promise((resolve, reject) => {
-        window.setTimeout(resolve, ms);
-    });
+//===============================================================================
+var counter = 0;
+document.getElementsByTagName('div')[0].addEventListener('click', print_fib);
+function print_fib() {
+    counter += 1;
+    console.log(fib(counter));
 }
-function teczoweKolory(el) {
-    console.log("here");
-    setTimeout(function () {
-        console.log('red');
-        el.style.backgroundColor = 'red';
-        setTimeout(function () {
-            el.style.backgroundColor = 'orange';
-            setTimeout(function () {
-                el.style.backgroundColor = 'yellow';
-                setTimeout(function () {
-                    el.style.backgroundColor = 'green';
-                    setTimeout(function () {
-                        el.style.backgroundColor = 'blue';
-                        setTimeout(function () {
-                            el.style.backgroundColor = 'indigo';
-                            setTimeout(function () {
-                                el.style.backgroundColor = 'purple';
-                            }, 1000);
-                        }, 1000);
-                    }, 1000);
-                }, 1000);
-            }, 1000);
-        }, 1000);
-    }, 1000);
+function fib(n) {
+    if (n < 2)
+        return n;
+    return fib(n - 1) + fib(n - 2);
 }
-function teczoweKoloryPromise(el) {
-    let promise = wait(1000);
-    for (const color of ['red', 'white', 'blue']) {
-        promise = promise.then(function () {
-            el.style.backgroundColor = color;
-            return wait(1000);
-        });
-    }
+reserved_element.addEventListener('change', checkChoosenData);
+var popup = document.getElementsByClassName("reserved_flight")[0];
+popup.addEventListener("click", function () {
+    popup.style.display = "none";
+});
+document.querySelector('[type=submit]').addEventListener("click", function () {
+    event.preventDefault();
+    //  get flight info
+    var passegerName = document.getElementById('fname').value;
+    var dateControl = document.querySelector('input[type="date"]');
+    var date_obj = new Date(dateControl.value);
+    var month = date_obj.getUTCMonth() + 1;
+    var day = date_obj.getUTCDate();
+    var year = date_obj.getUTCFullYear();
+    var new_date = year + "/" + month + "/" + day;
+    var depart_list = document.getElementById('departureslist');
+    var depart_city = depart_list.options[depart_list.selectedIndex].text;
+    var arrival_list = document.getElementById('arrivals');
+    var arrival_city = arrival_list.options[arrival_list.selectedIndex].text;
+    var flight_details = "Wylot: " + depart_city + "\nPrzylot: " + arrival_city + "\nData: " + new_date;
+    popup.style.display = "block";
+    var popup_p = popup.firstElementChild;
+    popup_p.innerText = "Zarezerwowano lot\n" + flight_details;
+});
+function checkChoosenData(ev) {
+    if (checkCities() && checkDate() && checkNames())
+        document.querySelector('[type=submit]').removeAttribute('disabled');
+    else
+        document.querySelector('[type=submit]').setAttribute('disabled', 'yes');
 }
-const colorful_elem = document.getElementById('table_box');
-// teczoweKoloryPromise(colorful_elem);
-//  async, await
-function pokoloruj(ev) {
-    let target = ev.target;
-    let elem = this;
-    console.log(elem, target);
-    let currentColor = window.getComputedStyle(elem).getPropertyValue('background-color');
-    let [_, ...colorsAsText] = /rgb\((\d+),[^0-9]*(\d+),[^0-9]*(\d+)\)/.exec(currentColor);
-    let colors = [];
-    for (let i = 0; i < 3; i++)
-        colors[i] = (parseInt(colorsAsText[i]) + 0x20) % 256;
-    elem.style.backgroundColor = `rgb(${colors[0]},${colors[1]},${colors[2]})`;
+function checkCities() {
+    var wrongDeparture = document.getElementById('departureslist')[0].selected;
+    var wrongArrival = document.getElementById('arrivals')[0].selected;
+    return !(wrongDeparture || wrongArrival);
+}
+function checkDate() {
+    var dateControl = document.querySelector('input[type="date"]');
+    var choosenDate = new Date(dateControl.value);
+    var today = new Date();
+    return choosenDate.getTime() - today.getTime() >= 0;
+}
+function checkNames() {
+    var name = document.getElementById('fname').value;
+    return name.trim().indexOf(' ') != -1;
 }
--- airline_page/tt.ts
+++ airline_page/tt.ts
@@ -2,28 +2,6 @@ import {Builder, Capabilities, Key} from 'selenium-webdriver';
 import { expect } from 'chai';
 import { driver } from 'mocha-webdriver';
 
-describe('test_blocked_links', function () {
-
-    it('checks blocked links after submit', async function() {
-        this.timeout(20000);
-        await driver.get(`file://${process.cwd()}/first.html`);
-        driver.executeScript(`document.querySelector('input[type=date]').value = "2040-12-01";`);
-
-        await driver.find('input[name=fname]').sendKeys('Albert Einstain');
-        await driver.find('select[id=departureslist]').sendKeys('Warszawa', Key.RETURN);
-        await driver.find('select[id=arrivals]').sendKeys('London', Key.RETURN);
-
-        expect(await driver.find('input[type=submit]').then(() => true, () => false)).to.equal(true);
-        expect(await (await driver.find('input[type=submit]')).isEnabled()).to.equal(true);
-        await (await driver.find('input[type=submit]')).click();
-        expect(await driver.find('table_adjust').doClick().then(() => {
-                return true;
-            }, () => {
-                return false;
-            })).to.equal(false);
-    });
-})
-
 describe('test_submit_button', function () {
 
     it('Check submit button is disabled', async function() {
@@ -37,3 +15,32 @@ describe('test_submit_button', function () {
     });
 
 });
+
+describe('test_blocked_links', function () {
+
+    it('checks blocked links after submit', async function() {
+        this.timeout(20000);
+        await driver.get(`file://${process.cwd()}/first.html`);
+
+        driver.executeScript(`
+            const today = new Date();
+            const tomorrow = new Date(today);
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            document.querySelector('input[type=date]').value = tomorrow.toJSON().slice(0,10).replace(/-/g,'-');
+        `);
+
+        await driver.find('input[name=fname]').sendKeys('Albert Einstain');
+        await driver.find('select[id=departureslist]').sendKeys('Warszawa', Key.RETURN);
+        await driver.find('select[id=arrivals]').sendKeys('London', Key.RETURN);
+
+        expect(await driver.find('input[type=submit]').then(() => true, () => false)).to.equal(true);
+        expect(await (await driver.find('input[type=submit]')).isEnabled()).to.equal(true);
+        await (await driver.find('input[type=submit]')).click();
+        expect(await driver.find('table_adjust').doClick().then(() => {
+                return true;
+            }, () => {
+                return false;
+            })).to.equal(false);
+
+    });
+})
